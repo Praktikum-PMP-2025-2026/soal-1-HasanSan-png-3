3103808e32b2a644e93de49c516477c48f8c5124
HasanSan-png <hasan.mutawakkil.al@gmail.com>
2026-05-11 09:59:05 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,131 +1,103 @@
-    //** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
-    //* Modul : 05 - Foundation of Algorithm
-    //* Hari dan Tanggal : Senin, 11 May 2026
-    //* Nama (NIM) : Hasan Al Mutawakkil (13224080)
-    //* Nama File : TP_5_PMP.c
-    //* Deskripsi : Mencari Leluhur
+//** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+//* Modul : 05 - Foundation of Algorithm
+//* Hari dan Tanggal : Senin, 11 May 2026
+//* Nama (NIM) : Hasan Al Mutawakkil (13224080)
+//* Nama File : TP_5_PMP.c
+//* Deskripsi : Mencari Leluhur (Analisis Graph Degree)
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <stdbool.h>
-    #include <string.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
 
-    struct Node {
-        int data;
-        struct Node* next;
-    };
+struct List {
+    struct Node* head;
+};
 
-    struct List {
-        struct Node* head;
-    };
+struct Graph {
+    int vertices;
+    struct List* array;
+};
 
-    struct Graph {
-        int vertices;
-        struct List* array;
-    };
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
 
-    struct Node* createNode(int data) {
-        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-        newNode->data = data;
-        newNode->next = NULL;
-        return newNode;
+struct Graph* createGraph(int vertices) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->vertices = vertices;
+    graph->array = (struct List*)malloc(vertices * sizeof(struct List));
+    for (int i = 0; i < vertices; i++) {
+        graph->array[i].head = NULL;
     }
+    return graph;
+}
 
-    struct Graph* createGraph(int vertices) {
-        struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
-        graph->vertices = vertices;
-        graph->array = (struct List*)malloc(vertices * sizeof(struct List));
-        for (int i = 0; i < vertices; i++) {
-            graph->array[i].head = NULL;
-        }
-        return graph;
-    }
+void addEdge(struct Graph* graph, int src, int dest) {
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->array[src].head;
+    graph->array[src].head = newNode;
+}
 
-    void addEdge(struct Graph* graph, int src, int dest) {
-        struct Node* newNode = createNode(dest);
-        newNode->next = graph->array[src].head;
-        graph->array[src].head = newNode;
+// ... (Fungsi DFS dan DFSTraversal tidak digunakan di main() saat ini, 
+//      tapi dibiarkan sesuai kode asli)
 
-
-    }
-
-    void DFS(struct Graph* graph, int vertex, bool visited[]) {
-        visited[vertex] = true;
-        printf("%d ", vertex);
-        struct Node* currentNode = graph->array[vertex].head;
-        while (currentNode) {
-            int adjacentVertex = currentNode->data;
-            if (!visited[adjacentVertex]) {
-                DFS(graph, adjacentVertex, visited);
-            }
-            currentNode = currentNode->next;
-        }
-    }
-
-    void DFSTraversal(struct Graph* graph, int* order, int orderSize) {
-        bool* visited = (bool*)malloc(graph->vertices * sizeof(bool));
-        for (int i = 0; i < graph->vertices; i++) {
-            visited[i] = false;
-        }
-        for (int i = 0; i < orderSize; i++) {
-            if (!visited[order[i]]) {
-                DFS(graph, order[i], visited);
-            }
-        }
-        free(visited);
-    }
-
-    int main() {
-        int n;
-        int arr[99]; 
-        scanf("%d", &n);
-
-
-        struct Graph* g = createGraph(n);
-        int matrix[n][n];
-        int degree[n];
-
-        for (int i = 0; i < n; i++) {
-        degree[i] = 0;
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                scanf("%d", &matrix[i][j]);
-                if (matrix[i][j] == 1) {
-                    addEdge(g, i, j);
-                    degree[i]++; 
-                }
-            }
-        }
-        
-        for(int i=0; i<n; i++){
-            printf("DEGREE %d %d\n", i, degree[i]);
-        }
-
-        
-
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) return 0;
     
+    struct Graph* g = createGraph(n);
+    int matrix[n][n];
+    int degree[n];
+    
+    for (int i = 0; i < n; i++) {
+        degree[i] = 0;
+    }
 
-
-        int maxD = -1, maxV =-1;
-        for(int i=0; i<n; i++){
-            if(degree[i] > maxD){
-                maxD = degree[i];
-                maxV = i;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+            if (matrix[i][j] == 1) {
+                addEdge(g, i, j);
+                degree[i]++;
             }
         }
-        printf("MAX_VERTEX %d", maxV);
-        printf("\n");
-        int c = -1;
-        bool foundIsolated = false;
-        printf("ISOLATED");
-        for(int i=0; i<n; i++){
-            if(degree[i] == 0){
-            printf(" %d", i);
-            foundIsolated = true;
-        }
+    }
 
+    // Output DEGREE
+    for(int i=0; i<n; i++){
+        printf("DEGREE %d %d\n", i, degree[i]);
+    }
+
+    // Cari Max Degree
+    int maxD = -1, maxV = -1;
+    for(int i=0; i<n; i++){
+        if(degree[i] > maxD){
+            maxD = degree[i];
+            maxV = i;
+        }
+    }
+    // Tambahkan \n agar MAX_VERTEX tidak menempel dengan ISOLATED
+    printf("MAX_VERTEX %d\n", maxV);
+
+    // Cari Isolated
+    bool foundIsolated = false;
+    for(int i=0; i<n; i++){
+        if(degree[i] == 0){
+            if (!foundIsolated) {
+                printf("ISOLATED");
+                foundIsolated = true;
+            }
+            printf(" %d", i);
+        }
     }
 
     if(!foundIsolated){
@@ -133,13 +105,5 @@
     }
     printf("\n");
 
-       // if (c == -1) {
-            //printf("ISOLATED NONE\n");
-        //} else {
-           // printf("ISOLATED %d\n", c);
-        //}
-
-
-
     return 0;
-    }
+}
